2fb67076f9fd6ea62394a37058f76a0b53c6f31f
Heitor Santos <heitor.santosprog@gmail.com>
2021-11-04 12:56:23 -0300

    Iniciando no framework Django

---
+++ receitasnodjango/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('receitas.urls')),
+]
