68ad83e7d3b78127545440d67e5ba1c79e1975de
Heitor Santos <heitor.santosprog@gmail.com>
2021-11-05 15:27:31 -0300

    nova atualização do projeto no django

--- receitasnodjango/settings.py
+++ receitasnodjango/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'receitasnodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'receita_django',
+        'USER': 'postgres',
+        'PASSWORD': 'heitor@123H',
+        'HOST': 'localhost',
+
     }
 }
 
